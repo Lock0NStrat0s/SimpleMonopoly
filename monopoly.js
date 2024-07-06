978b282eaf00d99922cdcd4b3d7a15636dd54fd2
Mohammad Jabir <mohammadj94@gmail.com>
2024-07-06 17:42:05 -0600

    dice roll and player movement implemented.

--- monopoly.js
+++ monopoly.js
@@ -20,11 +20,19 @@ d6.src = "./images/6.png";
 
 window.onload = function () {
   GameBoard();
+  document.querySelector("#RollDice").onclick = DiceRoll;
 };
 
 // globals
+var currLocP1 = 0;
+var currLocP2 = 0;
 var p1Money = 3000;
 var p2Money = 3000;
+var total = 0;
+var flag = 0;
+
+const takeAChanceText = ["Second Place in Beauty Contest: $10", "Bank Pays You Dividend of $50", "Repair your Properties. You owe $250", "Speeding Fine: $15", "Holiday Fund Matures: Receive $100", "Pay Hospital Fees: $100"];
+const takeAChanceMoney = [10, 50, -250, -15, 100, -100];
 
 function GameBoard() {
   // load player models
@@ -73,4 +81,107 @@ function GameBoard() {
   document.querySelector("#player1div").style.border = "10px solid teal";
 
   console.log("help");
+}
+
+function DiceRoll() {
+  var dice1 = Math.floor(Math.random() * 6) + 1;
+  var dice2 = Math.floor(Math.random() * 6) + 1;
+  total = dice1 + dice2;
+  // total = 5;
+
+  //load dies
+  var die1 = document.querySelector("#die01");
+  die1.height = 50;
+  var die2 = document.querySelector("#die02");
+  die2.height = 50;
+  document.querySelector(".buffer").innerHTML = "ROLL: " + total;
+
+  if (dice1 == 1) {die1.src = d1.src;}
+  else if (dice1 == 2) {die1.src = d2.src;}
+  else if (dice1 == 3) {die1.src = d3.src;}
+  else if (dice1 == 4) {die1.src = d4.src;}
+  else if (dice1 == 5) {die1.src = d5.src;}
+  else if (dice1 == 6) {die1.src = d6.src;}
+
+  if (dice2 == 1) {die2.src = d1.src;}
+  else if (dice2 == 2) {die2.src = d2.src;}
+  else if (dice2 == 3) {die2.src = d3.src;}
+  else if (dice2 == 4) {die2.src = d4.src;}
+  else if (dice2 == 5) {die2.src = d5.src;}
+  else if (dice2 == 6) {die2.src = d6.src;}
+
+  //whose turn is it?
+  if (flag == 0){
+      currLocP1 = PlayerMove(currLocP1, play1);
+      document.querySelector("#player1div").style.border = "none";
+      document.querySelector("#player2div").style.border = "10px solid maroon";
+      if (dice1 == dice2) {
+          flag = 0;
+      }
+      else {
+          flag = 1;
+      }
+  }
+  else if (flag == 1) {
+      currLocP2 = PlayerMove(currLocP2, play2);
+      document.querySelector("#player2div").style.border = "none";
+      document.querySelector("#player1div").style.border = "10px solid teal";
+      if (dice1 == dice2) {
+          flag = 1;
+      }
+      else {
+          flag = 0;
+      }
+  }
+
+  //update player money
+  document.querySelector("#player1amt").innerHTML = "$" + p1Money;
+  document.querySelector("#player2amt").innerHTML = "$" + p2Money;
+
+  // GAME END
+  if (p1Money < 0 ){
+      alert("PLAYER 1 LOSES!");
+      document.querySelector("#RollDice").disabled = true;
+      console.log("PLAYER 1 LOSES!");
+  }
+  if (p2Money < 0 ){
+      alert("PLAYER 2 LOSES!");
+      document.querySelector("#RollDice").disabled = true;
+      console.log("PLAYER 2 LOSES!");
+  }
+}
+
+function PlayerMove(locP, player) {
+  const speed = 100;
+  var timer;
+  var secAccess = document.querySelectorAll("section");
+  var loc;
+
+  if (locP + total < 40){
+      for (var i = locP; i <= locP + total; i++) {
+          secAccess[i].appendChild(player);
+          loc = i;
+      }
+  }
+  else {
+      for (var i = locP; i <= 39; i++) {
+          secAccess[i].appendChild(player);
+      }
+      for (var i = 0; i <= locP + total - 40; i++) {
+          secAccess[i].appendChild(player);
+          loc = i;
+      }
+      if (flag == 0){
+          p1Money -= parseInt(secAccess[0].attributes.val.value);
+      }
+      else if (flag == 1) {
+          p2Money -= parseInt(secAccess[0].attributes.val.value);
+      }
+      alert("You passed GO! You get $200");
+  }
+
+  locP = loc;
+  //locP = Interact(secAccess, locP, player);
+
+  return locP;
 }
